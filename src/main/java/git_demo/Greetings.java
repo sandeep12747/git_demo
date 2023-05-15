fcf93b21318f133698058c1065b6fbdf9970a857
sandeep singh <sandeep12747@gmail.com>
2023-05-15 17:43:09 +0530

    added git-demo project to local repo

---
+++ src/main/java/git_demo/Greetings.java
@@ -0,0 +1,12 @@
+package git_demo;
+
+public class Greetings {
+	public static void main(String[] args) {
+		System.out.println("hi");
+	}
+	
+	public void greet(String message) {
+		System.out.println("Hello "+message);
+	}
+
+}
