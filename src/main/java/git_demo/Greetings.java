6c5b3d6f24ed2c9af2d56012d261607e186d1676
sandeep singh <sandeep12747@gmail.com>
2023-05-16 12:16:06 +0530

    comment added

--- src/main/java/git_demo/Greetings.java
+++ src/main/java/git_demo/Greetings.java
@@ -9,6 +9,8 @@ public class Greetings {
 	public static void greet(String message) {
 		System.out.println("Hello "+message);
 	}
+	/* this  method is added for addition of two numbers*/
+	
 	
 	public static Integer AddNumber(int num1,int num2) {
 		
